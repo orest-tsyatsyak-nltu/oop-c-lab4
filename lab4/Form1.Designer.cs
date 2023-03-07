0841e1551d33de3b00b13780c06c0d491166aff5
Orest Tsiatsiak <oresttsytsyak02@gmail.com>
2023-03-07 10:23:02 +0200

    Add project files.

---
+++ lab4/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rows = new System.Windows.Forms.Label();
+            this.rowsField = new System.Windows.Forms.TextBox();
+            this.colsField = new System.Windows.Forms.TextBox();
+            this.cols = new System.Windows.Forms.Label();
+            this.maxField = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.minField = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.generateArray = new System.Windows.Forms.Button();
+            this.matrix = new System.Windows.Forms.DataGridView();
+            this.calculations = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.matrix)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rows
+            // 
+            this.rows.AutoSize = true;
+            this.rows.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rows.Location = new System.Drawing.Point(33, 29);
+            this.rows.Name = "rows";
+            this.rows.Size = new System.Drawing.Size(60, 25);
+            this.rows.TabIndex = 0;
+            this.rows.Text = "Rows";
+            // 
+            // rowsField
+            // 
+            this.rowsField.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rowsField.Location = new System.Drawing.Point(99, 27);
+            this.rowsField.Name = "rowsField";
+            this.rowsField.Size = new System.Drawing.Size(72, 27);
+            this.rowsField.TabIndex = 1;
+            // 
+            // colsField
+            // 
+            this.colsField.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.colsField.Location = new System.Drawing.Point(99, 70);
+            this.colsField.Name = "colsField";
+            this.colsField.Size = new System.Drawing.Size(72, 27);
+            this.colsField.TabIndex = 3;
+            // 
+            // cols
+            // 
+            this.cols.AutoSize = true;
+            this.cols.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cols.Location = new System.Drawing.Point(33, 69);
+            this.cols.Name = "cols";
+            this.cols.Size = new System.Drawing.Size(52, 25);
+            this.cols.TabIndex = 2;
+            this.cols.Text = "Cols";
+            // 
+            // maxField
+            // 
+            this.maxField.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.maxField.Location = new System.Drawing.Point(316, 70);
+            this.maxField.Name = "maxField";
+            this.maxField.Size = new System.Drawing.Size(72, 27);
+            this.maxField.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(208, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Max value";
+            // 
+            // minField
+            // 
+            this.minField.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.minField.Location = new System.Drawing.Point(316, 27);
+            this.minField.Name = "minField";
+            this.minField.Size = new System.Drawing.Size(72, 27);
+            this.minField.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(208, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Min value";
+            // 
+            // generateArray
+            // 
+            this.generateArray.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.generateArray.Location = new System.Drawing.Point(448, 13);
+            this.generateArray.Name = "generateArray";
+            this.generateArray.Size = new System.Drawing.Size(283, 50);
+            this.generateArray.TabIndex = 8;
+            this.generateArray.Text = "Generate Array";
+            this.generateArray.UseVisualStyleBackColor = true;
+            this.generateArray.Click += new System.EventHandler(this.generateArray_Click);
+            // 
+            // matrix
+            // 
+            this.matrix.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.matrix.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.matrix.Location = new System.Drawing.Point(38, 133);
+            this.matrix.Name = "matrix";
+            this.matrix.Size = new System.Drawing.Size(693, 296);
+            this.matrix.TabIndex = 9;
+            // 
+            // calculations
+            // 
+            this.calculations.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.calculations.Location = new System.Drawing.Point(448, 70);
+            this.calculations.Name = "calculations";
+            this.calculations.Size = new System.Drawing.Size(283, 50);
+            this.calculations.TabIndex = 10;
+            this.calculations.Text = "Do Calculations";
+            this.calculations.UseVisualStyleBackColor = true;
+            this.calculations.Click += new System.EventHandler(this.calculations_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.calculations);
+            this.Controls.Add(this.matrix);
+            this.Controls.Add(this.generateArray);
+            this.Controls.Add(this.maxField);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.minField);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.colsField);
+            this.Controls.Add(this.cols);
+            this.Controls.Add(this.rowsField);
+            this.Controls.Add(this.rows);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.matrix)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label rows;
+        private System.Windows.Forms.TextBox rowsField;
+        private System.Windows.Forms.TextBox colsField;
+        private System.Windows.Forms.Label cols;
+        private System.Windows.Forms.TextBox maxField;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox minField;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button generateArray;
+        private System.Windows.Forms.DataGridView matrix;
+        private System.Windows.Forms.Button calculations;
+    }
+}
+
---
+++ lab4/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab4
+{
+    public partial class Form1 : Form
+    {
+
+        private Random random = new Random();
+
+        private int[,] matrixValues;
+
+        private int currentMin = 0;
+
+        private int currentMax = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void generateArray_Click(object sender, EventArgs e)
+        {
+            int rowsCount = 0;
+            int colsCount = 0;
+            try 
+            {
+                GetUserInputs(ref rowsCount, ref colsCount);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+            FillMatrix(rowsCount, colsCount);
+            ShowMatrixInWindow(rowsCount, colsCount);
+        }
+
+        private void GetUserInputs(ref int rowsCount, ref int colsCount)
+        {
+            rowsCount = int.Parse(rowsField.Text);
+            colsCount = int.Parse(colsField.Text);
+            validateInputs(rowsCount, colsCount);
+            currentMin = int.Parse(minField.Text);
+            currentMax = int.Parse(maxField.Text);
+        }
+
+        private void validateInputs(int rowsCount, int colsCount)
+        {
+            if (rowsCount < 1 || colsCount < 1)
+            {
+                throw new ArgumentException("Rows and cols both must be greater than 0");
+            }
+        }
+
+        private void FillMatrix(int rowsCount, int colsCount)
+        {
+            matrixValues = new int[rowsCount, colsCount];
+            for (int rowI = 0; rowI < matrixValues.GetLength(0); rowI++)
+            {
+                for (int colI = 0; colI < matrixValues.GetLength(1); colI++)
+                {
+                    matrixValues[rowI, colI] = random.Next(currentMin, currentMax);
+                }
+            }
+        }
+
+        private void ShowMatrixInWindow(int rowsCount, int colsCount)
+        {
+            matrix.RowCount = rowsCount;
+            matrix.ColumnCount = colsCount;
+            for (int rowI = 0; rowI < matrixValues.GetLength(0); rowI++)
+            {
+                for (int colI = 0; colI < matrixValues.GetLength(1); colI++)
+                {
+                    matrix.Rows[rowI].Cells[colI].Style.BackColor = Color.White;
+                    matrix.Rows[rowI].Cells[colI].Value = matrixValues[rowI, colI];
+                }
+            }
+            matrix.Show();
+        }
+
+        private void calculations_Click(object sender, EventArgs e)
+        {
+            int sumOfRowsThatDoNotHaveNegativeNumbers = CountSumOfRowsThatDoNotHaveNegativeNumbers();
+            int minSumDiagonalSumAmongParalelToMainOne = FindMinDiagonalSumAmongParalelToMainOne();
+            MessageBox.Show("1 sub task: " + sumOfRowsThatDoNotHaveNegativeNumbers + "\n" +
+                "2 sub task: " + minSumDiagonalSumAmongParalelToMainOne);
+        }
+
+        private int CountSumOfRowsThatDoNotHaveNegativeNumbers()
+        {
+            int count = 0;
+            for (int rowI = 0; rowI < matrixValues.GetLength(0); rowI++)
+            {
+                int rowCount = 0;
+                bool negativeNumberWasFound = false;
+                for (int colI = 0; colI < matrixValues.GetLength(1); colI++)
+                {
+                    if (matrixValues[rowI, colI] < 0)
+                    {
+                        negativeNumberWasFound = true;
+                        break;
+                    }
+                    else
+                    {
+                        rowCount += matrixValues[rowI, colI];
+                    }
+                }
+                if(!negativeNumberWasFound)
+                {
+                    count += rowCount;
+                }
+            }
+            return count;
+        }
+
+        private int FindMinDiagonalSumAmongParalelToMainOne()
+        {
+            const int FIXED_STEP = 2;
+
+            int minSum = currentMax * matrixValues.GetLength(1);
+
+            int rowIStarter = 0;
+            int colIStarter = 0;
+            int diagonalsParalelTomainCounter = matrixValues.GetLength(0);
+
+            if(matrixValues.GetLength(0) % 2 == 0)
+            {
+                diagonalsParalelTomainCounter = matrixValues.GetLength(0) - 1;
+                rowIStarter = 1;
+            }
+
+            for (int diagonalCounter = 0; diagonalCounter < diagonalsParalelTomainCounter; diagonalCounter++)
+            {
+                int currentDiagonalSum = 0;
+
+                for(int colI = colIStarter, rowI = rowIStarter; colI != rowIStarter + 1; colI++, rowI--)
+                {
+                    currentDiagonalSum += matrixValues[rowI, colI];
+                    matrix.Rows[rowI].Cells[colI].Style.BackColor = Color.Red;
+                }
+
+                if(currentDiagonalSum < minSum)
+                {
+                    minSum = currentDiagonalSum;
+                }
+
+                if (rowIStarter + FIXED_STEP <= matrixValues.GetLength(0) - 1)
+                {
+                    rowIStarter += FIXED_STEP;
+                }
+                else 
+                {
+                    colIStarter += FIXED_STEP;
+                }
+            }
+            return minSum;
+        }
+
+    }
+}
